9a79dc27d78cf3bb66d769c33c39b85214ebb3b0
Brandsatz <59695429+Brandsatz@users.noreply.github.com>
2020-11-14 17:08:27 +0100

    meine Lösung zu Aufgabe 2
    Aufagbe 2 Video

--- Projekte/Video_2/Aufgabe_2/red_glove.py
+++ Projekte/Video_2/Aufgabe_2/red_glove.py
@@ -1,18 +1,46 @@
 import numpy as np
 import cv2
 
+# Callback Funktion für Slider, sie tut nichts
+def do_nothing():
+    return
+
 # Video aus Datei öffnen
 cap = cv2.VideoCapture('Micro-dance_2_.avi')
 
-# Rot, Grün und Blau Festwerte
+# drei Slider für Threshold 
+cv2.namedWindow("Video")
+cv2.createTrackbar("Blau", "Video", 5, 40, do_nothing)
+cv2.createTrackbar("Gruen", "Video", 10, 40, do_nothing)
+cv2.createTrackbar("Rot", "Video", 10, 40, do_nothing)
 
-r = 82
-g = 34
-b = 7
+# Rot, Grün und Blau Festwerte
+blauWert = 18
+gruenWert = 34
+rotWert = 82
+
 
 while cap.isOpened():
     ret, frame = cap.read()
 
+    # Video in drei Farbkanäle splitten
+    b, g, r = cv2.split(frame)
+
+    # Übergabe der Sliderwerte
+    b_Threshold = cv2.getTrackbarPos("Blau", "Video")
+    g_Threshold = cv2.getTrackbarPos("Gruen", "Video")
+    r_Threshold = cv2.getTrackbarPos("Rot", "Video")
+
+    # Farbmasken berechnen
+
+    b_Mask = cv2.inRange(b, blauWert-b_Threshold, blauWert+b_Threshold)
+    g_Mask = cv2.inRange(g, gruenWert-g_Threshold, gruenWert+g_Threshold)
+    r_Mask = cv2.inRange(r, rotWert-r_Threshold, rotWert+r_Threshold)
+
+
+    # Multiplikation der Einzelmasken
+
+    mask = r_Mask * g_Mask * b_Mask
 
 
     # Maske darstellen
