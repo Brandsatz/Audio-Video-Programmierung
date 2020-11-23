1324f97a6af58671949e6ec8dc67e38a7368faa4
Brandsatz <59695429+Brandsatz@users.noreply.github.com>
2020-11-23 15:47:24 +0100

    little change
    on reverb

--- Projekte/Audio_3/Aufgabe_A3b/reverb.js
+++ Projekte/Audio_3/Aufgabe_A3b/reverb.js
@@ -1,2 +1,46 @@
 
+let convoler = context.createConvolver();
+let reverbSelectlist = document.querySelector("reverbSelectList");
+let reverbOn = true;
+
+loadImpulseResponse("room");
+
+
+document.querySelector("#reverbSelectList").addEventListener("change", function (e) {
+    let name = e.target.options[e.target.selectedIndex].value;
+    loadImpulseResponse(name);
+});
+
+
+
+function loadImpulseResponse(name) {
+    if(reverbOn){
+        fetch('impulseResponses/' + name + '.wav')                   //Hier nochmal Dateipfad korrigierenn!!!
+            .then(response => response.arrayBuffer())
+            .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+            .then(audioBuffer => {
+                if (convoler) {convoler.disconnect();}
+
+                convoler = context.createConvolver();
+                convoler.buffer = audioBuffer;
+                convoler.normalize = true;
+
+                convoler.connect(context.destination);
+            })
+            .catch(console.error);
+    }
+};
+
+document.querySelector("#reverbOnOffButton").addEventListener("click", function(e){
+    if(reverbOn){
+        reverbOn = false;
+        convoler.disconnect();
+        document.querySelector("#reverbOnOffButton").innerHTML = "Turn on"
+    }else{
+        reverbOn = true;
+        let name = document.querySelector("#reverbSelectList").value;
+        loadImpulseResponse(name);
+        document.querySelector("#reverbOnOffButton").innerHTML = "Turn off"
+    }
+});
 
