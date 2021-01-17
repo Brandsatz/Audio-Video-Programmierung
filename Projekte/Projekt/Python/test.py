040520eccabddccf606549980d3a421e5d74f9d4
HB97 <henrike.bohnet@haw-hamburg.de>
2021-01-17 15:05:02 +0100

    Update test.py

--- Projekte/Projekt/Python/test.py
+++ Projekte/Projekt/Python/test.py
@@ -14,12 +14,12 @@ msg = inport.receive()
 if(msg):
     cap = cv2.VideoCapture(0)
     while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow("Origi", frame)
-    #time.sleep(0.5)
+        ret, frame = cap.read()
+        cv2.imshow("Origi", frame)
+        #time.sleep(0.5)
 
-    if cv2.waitKey(25) != -1:
-        break
+        if cv2.waitKey(25) != -1:
+            break
 
     print("Message recieved")
     print(msg)
